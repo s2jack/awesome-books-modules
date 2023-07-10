42f046af017fd690311020796333a6cfde28073f
Mehmet Selçuk Güler <gercekselcukguler@gmail.com>
2023-07-11 00:32:25 +0300

    test to import js files as module

---
+++ index.js
@@ -0,0 +1,5 @@
+import changeColor from "./modules/module01.js";
+
+const button = document.querySelector('.add-btn');
+
+button.addEventListener('click', changeColor);
---
+++ modules/module01.js
@@ -0,0 +1,5 @@
+const body = document.getElementsByTagName('body')[0];
+
+export default function changeColor () {
+  body.style.backgroundColor = 'red';
+}
